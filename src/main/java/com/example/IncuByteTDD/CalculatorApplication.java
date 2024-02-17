bd735ca87ca8050680ace1dcd52b1e542daaf666
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-02-17 13:04:54 +0530

    get numbers

--- src/main/java/com/example/IncuByteTDD/CalculatorApplication.java
+++ src/main/java/com/example/IncuByteTDD/CalculatorApplication.java
@@ -3,11 +3,18 @@ package com.example.IncuByteTDD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		Calculator calculator = new Calculator();
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		System.out.println("Enter the numbers");
+
+		String numbers = myObj.nextLine();  // Read user input
+		System.out.println("The sum is: " + calculator.add(numbers));  // Output user input
 	}
 
 }
