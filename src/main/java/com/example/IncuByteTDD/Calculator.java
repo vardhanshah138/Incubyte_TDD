3a44df363d7eb6701fa7fbec864f068abf128913
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-02-17 12:45:52 +0530

    return singular number

--- src/main/java/com/example/IncuByteTDD/Calculator.java
+++ src/main/java/com/example/IncuByteTDD/Calculator.java
@@ -3,6 +3,10 @@ package com.example.IncuByteTDD;
 public class Calculator {
 
     public Integer add (String string) {
-        return 0;
+        if(string.isEmpty()){
+            return 0;
+        }else{
+            return Integer.valueOf(string);
+        }
     }
 }
