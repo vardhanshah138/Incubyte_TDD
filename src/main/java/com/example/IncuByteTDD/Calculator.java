429608ff9c9b4e56632aae0f60848a4a433ce443
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-02-17 12:36:16 +0530

    Return Zero

---
+++ src/main/java/com/example/IncuByteTDD/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.IncuByteTDD;
+
+public class Calculator {
+
+    public Integer add (String string) {
+        return 0;
+    }
+}
--- src/test/java/com/example/IncuByteTDD/CalculatorApplicationTests.java
+++ src/test/java/com/example/IncuByteTDD/CalculatorApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.IncuByteTDD;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void shouldReturnZero() {
+		Calculator calculator = new Calculator();
+		Assertions.assertEquals(0, calculator.add(""));
 	}
 
 }
